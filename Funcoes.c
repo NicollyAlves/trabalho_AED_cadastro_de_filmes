50c9ca1b60f6ba8068dc9ef6287d95582d523405
souzagui <guilhermesnas@gmail.com>
2023-05-28 14:25:42 -0400

    add:tentativaCriarFilmes

--- Funcoes.c
+++ Funcoes.c
@@ -15,6 +15,11 @@ typedef struct filme{
     Info *informações; // Ponteiro para struct Info
 } Filme;
 
+
+void imprimir_filme(Filme* f){
+    printf("%.2f R$ || %s (%s, %d) || %s %d\n", f->preco, f->nome, f->informações->genero, f->informações->faixaEtaria,f->informações->diretor, f->informações->ano);
+}
+
 // Função que imprime a tabela atualizada dos filmes cadastrados
 // código -- preço R$ ## nome (gênero, faixa etária) ## diretor ano
 // 0001 -- 25.50 R$ ## The Godfather Part II (Crime/Drama, 16) ## Francis Ford Coppola 1974
@@ -25,5 +30,52 @@ void print_tabela(Filme **tabela, int tam){
         printf("%.4d -- ", i + 1); // código de 4 caracteres
         imprimir_filme(tabela[i]);
     }
-    menu();
+   // menu();
 }
+
+void clear_keyboard_buffer(void){
+    int c = 0;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+    return;
+}
+
+
+
+Filme* criar_filme(){
+    Filme* filme;
+    filme = malloc(sizeof(Filme));
+    filme->informações = malloc(sizeof(Info));
+
+    printf("Nome: ");
+    scanf("%[^\n]", filme->nome);
+    //fgets(filme->nome, 101, stdin);
+    getchar();
+    clear_keyboard_buffer();
+
+    printf("Preco: ");
+    scanf("%f", &filme->preco);
+    //clear_keyboard_buffer();
+
+    printf("Diretor: ");
+    scanf("%[^\n]", filme->informações->diretor);
+    //fgets(filme->informações->diretor, 101, stdin);
+    getchar();
+    clear_keyboard_buffer();
+
+    printf("Ano: ");
+    scanf("%d", &filme->informações->ano);
+    //clear_keyboard_buffer();
+
+    printf("Genero: ");
+    scanf("%[^\n]", filme->informações->genero);
+    //fgets(filme->informações->genero, 51, stdin);
+    getchar();
+    clear_keyboard_buffer();
+
+    printf("Faixa etaria: ");
+    scanf("%d", &filme->informações->faixaEtaria);
+    //clear_keyboard_buffer();
+
+    return filme;
+}
+
--- Main.c
+++ Main.c
@@ -1,6 +1,35 @@
-#include "Cabecalho.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "Cabecalho.h"
+
+typedef struct info{
+    char diretor[101]; // 101 caracteres + 1 para fim de string
+    char genero[51];   // 50 caracteres + 1 para fim de string
+    int ano;
+    int faixaEtaria; // Use 0 para classificação livre
+} Info;
+
+typedef struct filme{
+    char nome[101];    // 100 caracteres + 1 para fim de string
+    float preco;       // Até duas casas decimais
+    Info *informações; // Ponteiro para struct Info
+} Filme;
 
 int main(void){
+    int N;
 
+    printf("Entre com o tamanho da sua tabela: ");
+    scanf("%d", &N);
+
+    Filme **filmes;
+    filmes = (Filme**) malloc(N*sizeof(Filme**));
+
+    for(int i = 0; i < N; i++){
+        filmes[i] = (Filme*) malloc(sizeof(Filme*));
+        filmes[i] = criar_filme();
+    }
+
+    print_tabela(filmes, N);
 }
+
